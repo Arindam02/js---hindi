4dc56435c7fc988c4aa258faef31b3d420e1f8e6
Arindam02 <89266388+Arindam02@users.noreply.github.com>
2022-12-29 14:57:11 +0000

    Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict";       //treat all js code as newer version.
+
+// alert(3 + 3)        we are using nodejs not browser
+
+
+
+let name = "Arindam"
+let age = 23
+let islogIn = "False"
+
+// number =  2 to the power of 53
+// bigInt
+// boolean => true/false
+// string => "Arindam"
+// null => Standalone value
+// undefined => value not assign
+// symbol => uniqe
+
+
+// object
+
+
+console.log(typeof undefined);     //   undefined
+console.log(typeof null);          //   object
