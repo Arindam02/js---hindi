5990c99f44e86d9e338f83b098329d7c764aac8b
Arindam02 <89266388+Arindam02@users.noreply.github.com>
2022-12-30 14:54:13 +0000

    Convertion

---
+++ 01_basics/convertionOperation.js
@@ -0,0 +1,33 @@
+let score = "02ari"
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"02ari" => NaN (Not A Number)
+//true => 1 , false => 0
+
+let isLogIn = 1
+
+// console.log(typeof(isLogIn))
+
+let booleanIsLogIn = Boolean(isLogIn)
+// console.log(booleanIsLogIn);
+// console.log(typeof(booleanIsLogIn))
+
+
+// 1 => True , 0 => False
+// "" => False
+//"Arindam" => True
+
+
+let number = 14
+let stringNumber = String(number)
+
+console.log(stringNumber);
+console.log(typeof stringNumber)
