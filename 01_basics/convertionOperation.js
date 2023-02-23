5a022d555a16198c35fe7361992de31740ca7947
Arindam02 <89266388+Arindam02@users.noreply.github.com>
2023-02-23 08:27:00 +0000

    add operations.

--- 01_basics/convertionOperation.js
+++ 01_basics/convertionOperation.js
@@ -29,5 +29,46 @@ let booleanIsLogIn = Boolean(isLogIn)
 let number = 14
 let stringNumber = String(number)
 
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+
+
+// *************************************OPERATIONS********************************************
+
+let value = 3;
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(4/2);
+// console.log(4%2);
+
+
+
+let str1 = "hello"
+let str2 = " arindam"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
