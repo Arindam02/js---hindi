f516fb693916539bd2cef9b707b6ca42b2f5d7a2
Arindam02 <89266388+Arindam02@users.noreply.github.com>
2023-02-26 15:53:50 +0000

    numbs and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+let score = 400;
+// console.log(score)
+
+let balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const other_Number = 123.65678
+// console.log(other_Number.toPrecision(3));
+
+
+const hundreds = 10000000;
+// console.log(hundreds.toLocaleString("en-IN"))
+
+
+//++++++++++++++++++++++++++++++++++++++++----------MATHS-------------++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-9));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4 , 6 ,2 ,1 , 8));
+// console.log(Math.max(4,6,2,1,8));
+
+
+// console.log(Math.random());
+// console.log(Math.floor((Math.random()*6)+1));
+
+let min = 10;
+let max = 20;
+
+// console.log(Math.floor(Math.random() * (max-min +1))+min);
+console.log(Math.floor((Math.random() *10+1)+min));
